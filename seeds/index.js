3c50de2857ed210a88b877210f979bbd93a89b3c
Nishant <nishant.20235@knit.ac.in>
2023-03-29 21:00:17 +0530

    updated

--- seeds/index.js
+++ seeds/index.js
@@ -1,50 +1,50 @@
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
-const Campground = require('../models/campground');
+// const mongoose = require('mongoose');
+// const cities = require('./cities');
+// const { places, descriptors } = require('./seedHelpers');
+// const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    // useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useUnifiedTopology: true
-});
+// mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+//     // useNewUrlParser: true,
+//     // useCreateIndex: true,
+//     // useUnifiedTopology: true
+// });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+// const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
-const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            author: '641a1ff701dc855dca627895',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price,
-            images: [
-                {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
-                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
-                },
-                {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
-                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
-                }
-            ]
-        })
-        await camp.save();
-    }
-}
+// const seedDB = async () => {
+//     await Campground.deleteMany({});
+//     for (let i = 0; i < 50; i++) {
+//         const random1000 = Math.floor(Math.random() * 1000);
+//         const price = Math.floor(Math.random() * 20) + 10;
+//         const camp = new Campground({
+//             author: '641a1ff701dc855dca627895',
+//             location: `${cities[random1000].city}, ${cities[random1000].state}`,
+//             title: `${sample(descriptors)} ${sample(places)}`,
+//             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+//             price,
+//             images: [
+//                 {
+//                     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+//                     filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+//                 },
+//                 {
+//                     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+//                     filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+//                 }
+//             ]
+//         })
+//         await camp.save();
+//     }
+// }
 
-seedDB().then(() => {
-    mongoose.connection.close();
-})
+// seedDB().then(() => {
+//     mongoose.connection.close();
+// })
